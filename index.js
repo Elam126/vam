1b6a2be63b7f767d148bdec5e7d04b80b47260d4
Elam126 <41664953+Elam126@users.noreply.github.com>
2018-12-03 12:40:02 +0530

    comm

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
 
-app.get('/',(req,res) => {
-    res.send('<h2>Venzo Consultancy Services LLP</h2>');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri,{ useNewUrlParser: true }, (err) => {
+    if(err){
+         console.log('Database connectivity issue : ', err);
+    }else{
+         console.log('Connected to Database : '+ config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist/client'))
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
 
 app.listen(8080, () => {
